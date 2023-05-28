935b7d2e2d0354fcddf60a08c317d0fd9138aa99
Caserta <vitalage6@gmail.com>
2023-05-28 10:48:32 -0300

    implementação de gestos de deslizamento para navegação entre fragments

--- app/src/main/java/com/example/aplicativotcc/fragments/profileFragment.java
+++ app/src/main/java/com/example/aplicativotcc/fragments/profileFragment.java
@@ -1,66 +1,69 @@
 package com.example.aplicativotcc.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import com.example.aplicativotcc.R;
+import com.example.aplicativotcc.telaLogin;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link profileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class profileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public profileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment profileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static profileFragment newInstance(String param1, String param2) {
-        profileFragment fragment = new profileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private TextView NomeUser, EmailUser;
+    private Button bt_deslogar;
+    //Inicializa o Firebase com o nome db com a instância do mesmo
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Infla o layout do fragmento
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        NomeUser = view.findViewById(R.id.NomeUser);
+        EmailUser = view.findViewById(R.id.EmailUser);
+        bt_deslogar = view.findViewById(R.id.bt_deslogar);
+
+        //define um evento escutador que verifica se o botão deslogar foi acionado e depois cria uma intenção que nos manda para a tela de login
+        bt_deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), telaLogin.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    //Faz uma ligação com o db e busca os dados de usuário (nome, email), adiciona pra um documento de referência e depois adiociona para variáveis
+    //em STRING
+    public void onStart() {
+        super.onStart();
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        DocumentReference documentReference = db.collection("Usuários").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+                    NomeUser.setText(documentSnapshot.getString("nome"));
+                    EmailUser.setText(email.toString());
+                }
+            }
+        });
     }
+
 }
